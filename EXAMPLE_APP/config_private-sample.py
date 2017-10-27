3f8ca434000afb24e8bbe051a4169ff7e750503e
justinseibert <justinseibert@users.noreply.github.com>
2017-10-27 14:31:55 -0700

    adds api_key examples

--- EXAMPLE_APP/config_private-sample.py
+++ EXAMPLE_APP/config_private-sample.py
@@ -4,12 +4,17 @@ class DefaultConfig(object):
     # default config inherited by all
 
     SECRET_KEY = 'EXAMPLE_APP-secretkey'
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     DATABASE = os.path.join(PROJECT_ROOT, 'EXAMPLE_APP.db')
 
 class DevConfig(DefaultConfig):
+    DEV_API_PUBLIC_KEY = 'EXAMPLE_APP-development-api-public-key'
+    DEV_API_PRIVATE_KEY = 'EXAMPLE_APP-development-api-PRIVATE-key'
     pass
 
 class ProdConfig(DefaultConfig):
+    PROD_API_PUBLIC_KEY = 'EXAMPLE_APP-production-api-public-key'
+    PROD_API_PRIVATE_KEY = 'EXAMPLE_APP-production-api-PRIVATE-key'
     pass
 
 def get_config(MODE):
